b7edd0e37ed4a3e36e9007b13d326f60d270bd0c
hiro8watanabe <87076032+hiro8watanabe@users.noreply.github.com>
2022-06-06 18:00:19 +0900

    Router関係エラー解消

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
-import { HeaderOnly } from "./components/templates/HeaderOnly";
+// import { BrowserRouter } from "react-router-dom";
+// import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
+// import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+// import { SearchInput } from "./components/molecules/SearchInput";
+
+import { Router } from "./router/Router";
 import "./styles.css";
 
 const user = {
@@ -19,15 +18,5 @@ const user = {
 };
 
 export const App = () => {
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>ボタン</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 };
