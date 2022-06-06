26cb439ef06f5bf623faf37beb813c61deef34ed
hiro8watanabe <87076032+hiro8watanabe@users.noreply.github.com>
2022-06-06 09:02:45 +0900

    organisms Create for UserCard.jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "ひろ",
+  img: "https://source.unsplash.com/ymy29Y4J5Qs",
+  email: "12345@aaa.com",
+  tel: "000-9999-9999",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "https://google.com"
+};
 
 export const App = () => {
   return (
@@ -10,6 +22,7 @@ export const App = () => {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 };
