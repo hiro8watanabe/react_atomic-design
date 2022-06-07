0cf4bb1d165c9c46f26a511bae0e6dc381712e6b
hiro8watanabe <87076032+hiro8watanabe@users.noreply.github.com>
2022-06-07 11:46:36 +0900

    Create pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,5 @@
-// import { BrowserRouter } from "react-router-dom";
-// import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-// import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-// import { SearchInput } from "./components/molecules/SearchInput";
-
 import { Router } from "./router/Router";
 import "./styles.css";
-
-const user = {
-  name: "ひろ",
-  img: "https://source.unsplash.com/ymy29Y4J5Qs",
-  email: "12345@aaa.com",
-  tel: "000-9999-9999",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://google.com"
-};
 
 export const App = () => {
   return <Router />;
