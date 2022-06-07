9520d226059610b1e3f83d380b39f8f38e4b5ce9
hiro8watanabe <87076032+hiro8watanabe@users.noreply.github.com>
2022-06-07 12:54:35 +0900

    stateをバケツリレーする事例(非推奨)

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,9 +1,20 @@
 import styled from "styled-components";
+import { useHistory } from "react-router-dom";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
 
 export const Top = () => {
+  const History = useHistory();
+  const onClickAdmin = () =>
+    History.push({ pathname: "/users", state: { isAdmin: true } });
+  const onClickGeneral = () =>
+    History.push({ pathname: "/users", state: { isAdmin: false } });
   return (
     <SContainer>
       <h2>TOPページです</h2>
+      <SecondaryButton onClick={onClickAdmin}>管理者</SecondaryButton>
+      <br />
+      <br />
+      <SecondaryButton onClick={onClickGeneral}>ゲスト</SecondaryButton>
     </SContainer>
   );
 };
